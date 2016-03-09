6658583894d051f4c674db8afb7ca8a00f7e8353
lixiaoxf <lixiaoxf@126.com>
2016-03-09 17:56:15 +0800

    new

--- js/jq-lixiao-scroll-bar.js
+++ js/jq-lixiao-scroll-bar.js
@@ -2,12 +2,15 @@
  * Created by lixiao on 2016/2/17.
  */
 (function ($) {
-    var thisDiv;
-    var thisDivHeight;
-    var thisDivChildHeight;
-    var scrollBarDom;
-    //默认的选项
-    var defaultOption = {
+    var XiaoScrollBar = function(element,options){
+        this.options = null;
+        this.$element = null;
+        this.init(element,options)
+    }
+    XiaoScrollBar.VERSION = "1.0.0"
+
+    //默认参数
+    XiaoScrollBar.DEFAULTS = {
         "width":"15px",
         "position":"absolute",
         "top":0,
@@ -17,93 +20,108 @@
         "scrollLength":30
     };
 
-    //合并默认与用户设置后的参数对象
-    var opt;
+    //初始化
+    XiaoScrollBar.prototype.init = function(element,options){
+        this.$element = $(element);
+        this.options = $.extend({},XiaoScrollBar.DEFAULTS,options)
+        this.addXiaoScrollBarEvent()
+    }
+
+    //创建滚动条
+    XiaoScrollBar.prototype.createScrollBarView = function(){
+        if(this.isShowScrollBar()){
+            var scrollDom = this.$scrollDom || $("<div class='xiaoScrollBar' style='position:absolute;'></div>");
+            var scrollBarBlock = this.$scrollDom ?this.$scrollDom.find("div") : $("<div style='position:relative;top:0;' class='xiaoScrollBlock'></div>");
+            this.setscrollBarBlock(scrollBarBlock);
+            this.setscrollBar(scrollDom)
+            this.setScrollPosition(scrollDom);
+            scrollDom.append(scrollBarBlock);
+            if(!this.$scrollDom){
+                this.$scrollDom = scrollDom
+            }
+            this.$element.append(this.$scrollDom)
+        }
+    }
+
+    //销毁滚动条
+    XiaoScrollBar.prototype.destoryShowScrollBar = function(){
+        if(this.$scrollDom){
+            this.$element.find(this.$scrollDom).remove()
+        }
+    }
+
+    //设置滚动块的高度
+    XiaoScrollBar.prototype.setscrollBarBlock = function(scrollBarBlock){
+        var contentHeight = this.$element[0].scrollHeight;
+        var elementHeight = this.$element.height()
+        var height = (elementHeight/(contentHeight-elementHeight))*elementHeight;
+        scrollBarBlock.css({
+            "height":height,
+            "background":this.options.scrollBarBlockColor
+        });
+    };
+
+    //设置滚动条的高度
+    XiaoScrollBar.prototype.setscrollBar = function(scrollDom){
+        var height = this.options.height || this.$element.innerHeight();
+        scrollDom.css({
+            "width":this.options.width,
+            "height":height,
+            "background":this.options.background
+        });
+    };
+
+    //设置滚动条位置
+    XiaoScrollBar.prototype.setScrollPosition = function(scrollDom){
+        if(this.$element.css("position") == "static"){
+            this.$element.css("position","relative")
+        }
+        scrollDom.css({
+            "position":this.options.position,
+            "top":this.options.top+this.$element.scrollTop(),
+            "right":this.options.right,
+        });
+    };
 
     //判断当前的元素是否需要显示滚动条
-    function isShowScrollBar(){
-        if(!thisDiv){
+    XiaoScrollBar.prototype.isShowScrollBar = function(){
+        if(!this.$element){
             return false;
         }
-        if(thisDivHeight < thisDivChildHeight){
+        if(this.$element.innerHeight() < this.$element[0].scrollHeight){
             return true;
         }else{
             return false;
         }
     }
 
-    //参数初始化
-    function init(options){
-        thisDiv = $(this);
-        opt = $.extend({},defaultOption,options||{});
-        thisDivHeight = thisDiv.height();
-        thisDivChildHeight = thisDiv[0].scrollHeight;
+    //追加触发事件
+    XiaoScrollBar.prototype.addXiaoScrollBarEvent = function(){
+        var _this = this;
+        //鼠标移入移出 显示隐藏 滚动条
+        this.$element.bind("mouseenter",function(){
+            _this.createScrollBarView();
+        })
+
+        this.$element.bind("mouseleave",function(){
+            _this.destoryShowScrollBar();
+        })
+
+        //鼠标拖动滚动块
+        this.dragBlock();
+
+        //ie chrom ,火狐 鼠标滚轮事件不同
+        this.$element.bind("mousewheel DOMMouseScroll",function(e){
+            var direction = _this.mousewheelDirection(e);
+            _this.scrollXiaoScrollBar(direction);
+            _this.moveContentByBarBlock();
+            _this.setScrollBarPosition();
+            return false;
+        })
     }
 
-    //获取可视区外的高度
-    function getContentScrollHeight(){
-        return thisDiv[0].scrollHeight;
-    }
-
-    //设置滚动条位置
-    function setScrollBarPosition(scrollBarBg){
-        var height = opt.height || thisDiv.innerHeight();
-        //var top =
-        var thisDivPosition = thisDiv.css("position");
-        if(thisDiv.css("position") == "static"){
-            thisDiv.css("position","relative")
-        }
-        scrollBarBg.css({
-            "width":opt.width,
-            "height":height,
-            "position":opt.position,
-            "top":opt.top+thisDiv.scrollTop(),
-            "right":opt.right,
-            "background":opt.background
-        });
-    }
-    //设置滚动块大小
-    function setscrollBarBlockHeight(scrollBarBlock){
-        var height = (thisDivHeight/(thisDivChildHeight-thisDivHeight))*thisDivHeight;
-        scrollBarBlock.css({
-            "height":height,
-            "background":opt.scrollBarBlockColor
-        });
-    }
-
-    //创建控件视图
-    function createScrollBarView(){
-        scrollBarDom = $("<div class='xiaoScrollBar' style='display: none;position:absolute;'></div>");
-        var scrollBarBlock = $("<div style='position:relative;top:0;'></div>");
-        setscrollBarBlockHeight(scrollBarBlock);
-        setScrollBarPosition(scrollBarDom);
-        scrollBarDom.append(scrollBarBlock);
-        thisDiv.append(scrollBarDom)
-    }
-
-    //获取轨道长度
-    function getRailway(){
-        var block = scrollBarDom.find(">div");
-        return scrollBarDom.height()-block.innerHeight()
-    }
-
-    //滚动滚动条
-    function scrollXiaoScrollBar(direction){
-        var block = scrollBarDom.find(">div");
-        var blockTop = block.position().top;
-        var scrollLength = (opt.scrollLength/thisDivChildHeight)*scrollBarDom.height();
-        var afterScrollTop = blockTop+(scrollLength*direction);
-        if(afterScrollTop < 0){
-            afterScrollTop = 0;
-        }
-        if(afterScrollTop > scrollBarDom.height()-block.innerHeight()){
-            afterScrollTop = scrollBarDom.height()-block.innerHeight();
-        }
-        block.css("top",afterScrollTop)
-    }
-
-    //获取滚动方向
-    function mousewheelDirection(e){
+    //获取滚轮滚动方向
+    XiaoScrollBar.prototype.mousewheelDirection = function(e){
         var event = window.event || e;
         if(event.wheelDelta){
             return event.wheelDelta  < 0 ? 1 : -1
@@ -112,50 +130,98 @@
         }
     }
 
-    //根据滚动块位置移动内容主体
-    function moveContentByBarBlock(){
-        var barBlockTop =$(scrollBarDom).children("div").position().top;
-        var contentScrollTop = Math.ceil(barBlockTop/getRailway()*(getContentScrollHeight()-$(thisDiv).innerHeight()));
-        thisDiv.scrollTop(contentScrollTop);
+    //滚动滚动块
+    XiaoScrollBar.prototype.scrollXiaoScrollBar = function(direction){
+        var block = this.$scrollDom.find(".xiaoScrollBlock");
+        var blockTop = block.position().top;
+        var scrollLength = (this.options.scrollLength/this.$element[0].scrollHeight)*this.$scrollDom.height();
+        var afterScrollTop = blockTop+(scrollLength*direction);
+        if(afterScrollTop < 0){
+            afterScrollTop = 0;
+        }
+        if(afterScrollTop > this.$scrollDom.innerHeight()-block.innerHeight()){
+            afterScrollTop = this.$scrollDom.innerHeight()-block.innerHeight();
+        }
+        block.css("top",afterScrollTop)
+    }
+
+    //根据滚动块位置移动内容
+    XiaoScrollBar.prototype.moveContentByBarBlock = function(){
+        var barBlockTop =this.$scrollDom.children(".xiaoScrollBlock").position().top;
+        var contentScrollTop = Math.ceil(barBlockTop/this.getRailway()*(this.$element[0].scrollHeight-this.$element.innerHeight()));
+        this.$element.scrollTop(contentScrollTop);
+    }
+
+    //获取轨道长度
+    XiaoScrollBar.prototype.getRailway = function(){
+        var block = this.$scrollDom.find(".xiaoScrollBlock");
+        return this.$scrollDom.height()-block.innerHeight()
+    }
+
+    //设置滚动条在内容滚动后的位置
+    XiaoScrollBar.prototype.setScrollBarPosition = function(){
+        var height = this.options.height || this.$element.innerHeight();
+        var thisDivPosition = this.$element.css("position");
+        if(this.$element.css("position") == "static"){
+            this.$element.css("position","relative")
+        }
+        this.$scrollDom.css({
+            "width":this.options.width,
+            "height":height,
+            "position":this.options.position,
+            "top":this.options.top+this.$element.scrollTop(),
+            "right":this.options.right,
+            "background":this.options.background
+        });
     }
 
     //拖拽滚动块
-    function dragScrollBlock(){
-        var block = scrollBarDom.find(">div");
-        block.mousedown(function(e){
-            var event = e || window.event;
-            var mX = event.clientX;
-            var mY = event.clientY;
+    XiaoScrollBar.prototype.dragBlock = function(){
+        var _this = this;
+        var cY,isDrag = false;
+        //var $block = this.$scrollDom.find(".xiaoScrollBlock");
+        this.$element.on("mousedown",".xiaoScrollBlock",function(e){
+            cY = e.pageY;
+            isDrag = true;
         })
-    }
-
-    //声明的xiaoScrollBar控件
-    $.fn.xiaoScrollBar = function(options){
-        init.apply(this,options)
-        createScrollBarView();
-        thisDiv.bind("mouseover",function(){
-            if(isShowScrollBar()){
-                scrollBarDom.show();
-            }else{
-                scrollBarDom.hide();
+        this.$element.on("mousemove",function(e){
+            if(isDrag){
+                $(this).css("cursor","default");
+                var block = $(this).find(".xiaoScrollBlock")
+                var aY = e.pageY - cY;
+                cY = e.pageY;
+                var blockTop = block.position().top+aY;
+                if(blockTop<0){
+                    blockTop = 0;
+                }else if(blockTop > _this.$scrollDom.innerHeight()-block.innerHeight()){
+                    blockTop = _this.$scrollDom.innerHeight()-block.innerHeight()
+                }
+                block.css("top",blockTop);
+                _this.moveContentByBarBlock();
+                _this.setScrollBarPosition();
+                return false;
             }
         })
 
-        thisDiv.mouseleave(function(){
-            scrollBarDom.hide();
+        this.$element.on("mouseup mouseleave",function(e){
+            $(this).css("cursor","auto");
+            isDrag = false;
         })
 
-        dragScrollBlock();
-
-        //ie chrom ,火狐 鼠标滚轮事件不同
-        thisDiv.bind("mousewheel DOMMouseScroll",function(e){
-            var direction = mousewheelDirection(e);
-            scrollXiaoScrollBar(direction);
-            moveContentByBarBlock();
-            setScrollBarPosition(scrollBarDom);
-            return false;
-        })
-
+        //$block.mousemove(function(e){
+        //    var mX = e.pageX;
+        //    var mY = e.pageY;
+        //    console.log(mX+" "+mY)
+        //})
 
     }
+
+    function Plugin(option){
+        var opt = option || {};
+        new XiaoScrollBar(this,opt);
+    }
+
+    $.fn.xiaoScrollBar = Plugin
+    $.fn.xiaoScrollBar.Constructor = XiaoScrollBar;
 })(jQuery)
+
